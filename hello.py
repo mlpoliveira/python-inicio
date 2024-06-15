950b7d086c134ad846eb02c10a3a4ac8331fc703
Maria Oliveira <mlpoliveira@outlook.com>
2024-06-15 17:12:32 -0300

    Meu Primeiro Hello World Python

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 #! C:\Users\mlpol\AppData\Local\Microsoft\WindowsApps\
-#print ('Hello World!')
-#print ('maria'.upper())
-#print (56+3)
+  """Hello World Multi Linguas.
+  
+  Dependendo da Lingua configurada no ambiente o programa exibe a mensagem 
+  correspondente.
+  """
 # Este programa imprime Hello World
-print ('Hello World!') # test-ignore (comentario para ignorar uma linha do codigo TAG, para ferramentas de analise de codigo , testes)
+print ('Hello World!')
 
